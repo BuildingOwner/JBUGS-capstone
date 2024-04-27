2c8d52bfa0431bf0fea643556e609695fb80d2c4
jinsw4523 <jinsw4523@naver.com>
2024-04-27 16:46:33 +0900

    2024-04-27
    메인페이지 퀴즈 정보 추가

--- server/src/main/front/src/pages/coursePage/Course.js
+++ server/src/main/front/src/pages/coursePage/Course.js
@@ -19,7 +19,9 @@ const Course = () => {
   const [lectureName, setLectureName] = useState();
   const [division, setDivision] = useState();
   const [assignments, setAssignments] = useState([]);
+  const [quizs, setQuizs] = useState([]);
   const assignmentlist = "assignmentlist"
+  const quizlist = "quizlist"
 
 
   useEffect(() => {
@@ -27,16 +29,19 @@ const Course = () => {
       try {
         const response = await axios.get(`/api/course/${enrollmentId}`, {
           withCredentials: true // 세션 쿠키를 사용하기 위해 필요
-
         });
+        console.log("response : ", response)
         const lectureName1 = response.data.courseDto.lectureName
         const division1 = response.data.courseDto.division
-        console.log(lectureName1)
+        console.log("lectureName : ", lectureName1)
         setLectureName(lectureName1)
         setDivision(division1)
-        const assignmentData = response.data.weeklyContents.map((week) => week.assignments).flat();
-        setAssignments(assignmentData);
-        console.log(assignmentData)
+        const assignmentData = response.data.weeklyContents.map((week) => week.assignments).flat()
+        const quizData = response.data.weeklyContents.map((week) => week.quizzes).flat()
+        console.log("quiz Data : ", quizData)
+        setAssignments(assignmentData)
+        setQuizs(quizData)
+        console.log("assignmentData : ", assignmentData)
       }
       catch (error) {
         console.error("Error fetching course info:", error);
@@ -48,7 +53,7 @@ const Course = () => {
 
   return (
     <div className="course1">
-      <Sidebar lectureName={lectureName} division={division}/>
+      <Sidebar lectureName={lectureName} division={division} />
       <main className="serializer">
         <section className="content">
           <div className="nav">
@@ -93,9 +98,9 @@ const Course = () => {
                 <h2>과제</h2>
               </div>
               <div className="list-cjw no-scroll-bar">
-              {assignments.map((assignment) => (
+                {assignments.map((assignment) => (
                   <ListItem
-                    key={assignment.id}
+                    key={assignment.id} 
                     title={assignment.title}
                     contents={assignment.contents}
                     dueDate={assignment.dueDate}
@@ -112,15 +117,7 @@ const Course = () => {
                 <h2>자료</h2>
               </div>
               <div className="list-cjw no-scroll-bar">
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
+
               </div>
             </div>
             <div className="list-container-cjw">
@@ -128,15 +125,18 @@ const Course = () => {
                 <h2>퀴즈</h2>
               </div>
               <div className="list-cjw no-scroll-bar">
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
+                {quizs.map((quiz, i) => (
+                  <ListItem
+                    key={`quiz${i}`}
+                    quizName={quiz.quizName}
+                    quizType={quiz.quizType}
+                    deadline={quiz.deadline}
+                    weekId={quiz.weekId}
+                    submissionStatus={quiz.submissionStatus}
+                    enrollmentId={enrollmentId}
+                    url={quizlist}
+                  />
+                ))}
               </div>
             </div>
           </div>

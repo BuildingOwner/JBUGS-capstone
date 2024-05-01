ceb53f1133b676d4861da556b7b4bc9c1549e8c5
BuildingOwner <wodhks0911@gmail.com>
2024-05-01 19:37:32 +0900

    로그아웃 버튼

--- server/src/main/front/src/sidebar/MainCollepsedSidebar.js
+++ server/src/main/front/src/sidebar/MainCollepsedSidebar.js
@@ -17,12 +17,12 @@ function CollepsedSidebar() {
       </Link>
       <div className={styles.sidebarContainer}>
         <div className={styles.navBtns}>
-          <button className={`btn btn-primary ${styles.sidebarNavBtn}`}>
+          <button className={`btn btn-primary ${styles.sidebarNavBtn} ${styles2.sidebarNavBtn}`}>
             <Link to={"/main"} className={styles.linkBtn}>
               <GoHome size="30" />
             </Link>
           </button>
-          <button className={`btn btn-primary ${styles.sidebarNavBtn}`}>
+          <button className={`btn btn-primary ${styles.sidebarNavBtn} ${styles2.sidebarNavBtn}`}>
             <Link to={"/chatbotpage"} className={styles.linkBtn}>
               <IoChatbubbleEllipsesOutline size="30" />
             </Link>

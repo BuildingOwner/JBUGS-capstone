b6adc633454478a11049c0e6717f97a9fb8cb9f1
BuildingOwner <wodhks0911@gmail.com>
2024-05-17 16:51:51 +0900

    수정

--- server/src/main/front/src/modals/profModal/uploadModal/FileUploadModal.js
+++ server/src/main/front/src/modals/profModal/uploadModal/FileUploadModal.js
@@ -148,25 +148,6 @@ const FileUploadModal = (props) => {
           onClick={props.onRequestClose}>X</button>
       </div>
       <div className={`no-scroll-bar ${styles.gap}`}>
-
-        {/* <div className="car-wrapper">
-          <div className="car">
-            <b className="b75">{`주차 : `}</b>
-            <div className="menu-wrapper">
-              <select
-                aria-label="Small select example"
-                onChange={handleWeekChange}>
-                <option value={null}>주차 선택</option>
-                {Array.from({ length: 16 }).map((_, i) => {
-                  return (
-                    <option value={i + 1} key={`weekKey${i}`}>{i + 1} 주차</option>
-                  )
-                })}
-              </select>
-            </div>
-          </div>
-        </div> */}
-
         <div className={styles.contents}>
           <Info title={"주차 선택"} content={
             <select
@@ -258,73 +239,7 @@ const FileUploadModal = (props) => {
           <Info title={"퀴즈 설명"} content={<textarea className={`form-control`} rows={4} placeholder="퀴즈 설명" onChange={changeDescription}></textarea>} />
         </div>
 
-
-
-        {/* <section className="fileinput-parent">
-          <div className="fileinput"> */}
-        {/* <div className="file-name">
-              <b className="b76">{`자료명 : `}</b>
-              <textarea placeholder="자료명을 입력해주세요" onChange={changeFileTitle}></textarea>
-            </div>
-            <div className="navbtn">
-              <div className="wrapper16">
-                <input
-                  className='file-input'
-                  type="file"
-                  onChange={handleFileChange}
-                />
-              </div>
-            </div> */}
-        {/* <select
-              aria-label="Small select example"
-              onChange={changeChoice}>
-              <option value={null}>객관식 개수</option>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <option value={i + 1} key={`weekKey${i}`}>{i + 1}개</option>
-                )
-              })}
-            </select>
-            <select
-              aria-label="Small select example"
-              onChange={changeShortAnswer}>
-              <option value={null}>주관식 개수</option>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <option value={i + 1} key={`weekKey${i}`}>{i + 1}개</option>
-                )
-              })}
-            </select>
-            <select
-              aria-label="Small select example"
-              value={quizType}
-              onChange={handleTypeChange}>
-              <option value={null}>문제 유형</option>
-              <option value={"EXERCISE"}>연습 문제</option>
-              <option value={"PRACTICE"} >실습 문제</option>
-            </select> */}
-        {/* <textarea placeholder="퀴즈 설명" onChange={changeDescription}></textarea> */}
-
-        {/* </div>
-          <div className="fileinput1">
-            <div className="parent1">
-              <b className="b77">동영상명 :</b>
-              <textarea placeholder="자료명을 입력해주세요" onChange={changeVideoTitle}></textarea>
-            </div>
-            <div className="fileinput-inner">
-              <div className="wrapper17">
-                <input
-                  className='file-input'
-                  type="file"
-                  accept=".mp4, .mov"
-                  onChange={handleVideoChange}
-                />
-              </div>
-            </div>
-          </div>
-        </section> */}
       </div>
-      {/* <div className="fileuploadmodal-child" /> */}
       <div className={styles.bottom}>
         <button className={`btn btn-primary ${styles.closeBtn}`}
           onClick={props.onRequestClose}>닫기

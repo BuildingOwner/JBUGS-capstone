4f34a77e7751673f1e5cdf5b2d6e46860594e433
jangjuya <109902601+jangjuya@users.noreply.github.com>
2024-05-22 10:40:01 +0900

    env파일 추가

--- server/src/main/java/jbugs/eclass/EclassApplication.java
+++ server/src/main/java/jbugs/eclass/EclassApplication.java
@@ -1,5 +1,6 @@
 package jbugs.eclass;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EclassApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("MYSQL_HOST_PORT", dotenv.get("MYSQL_HOST_PORT"));
+		System.setProperty("USER_NAME", dotenv.get("USER_NAME"));
+		System.setProperty("USER_PASSWORD", dotenv.get("USER_PASSWORD"));
+		System.setProperty("file_dir", dotenv.get("file_dir"));
+
 		SpringApplication.run(EclassApplication.class, args);
 	}
 

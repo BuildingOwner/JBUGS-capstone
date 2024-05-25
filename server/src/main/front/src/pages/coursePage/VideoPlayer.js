cd618e6cb1b4bc68ed89523f667f5b7e5e04e6af
jinsw4523 <jinsw4523@naver.com>
2024-05-25 15:14:34 +0900

    퀴즈 모달 오류수정
    퀴즈 모달 오류수정

--- server/src/main/front/src/pages/coursePage/VideoPlayer.js
+++ server/src/main/front/src/pages/coursePage/VideoPlayer.js
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import styles from "./VideoPlayer.module.css"
+import { useState, useEffect } from "react"
 
 const VideoPlayer = (props) => {
+  const [videoUrl, setVideoUrl] = useState('');
+  const [mimeType, setMimeType] = useState('');
 
+  useEffect(() => {
+    const handleMessage = (event) => {
+      if (event.origin !== window.location.origin) return;
+
+      const { videoUrl, mimeType } = event.data;
+      console.log({videoUrl, mimeType})
+      setVideoUrl(videoUrl);
+      setMimeType(mimeType);
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
   return (
     <div className={styles.player}>
       <div className={styles.inner}>
-        <ReactPlayer url={props.videoUrl} />
+        <ReactPlayer
+          url={videoUrl}
+          playing={true} // 자동 재생 활성화
+          controls={true} // 비디오 컨트롤 표시
+          volume={0.8} // 초기 볼륨 설정 (0.0 ~ 1.0)
+          loop={true} // 비디오 루프 재생
+        />
       </div>
     </div>
   )
